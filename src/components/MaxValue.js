862fb1659cc2bc3cc81a0bc1cd09462bcfcf06fc
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-02 10:55:43 +0100

    react app created, folder setup, container & component skeletons added

---
+++ src/components/MaxValue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MaxValue() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MaxValue
---
+++ src/containers/MortgageContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MortgageContainer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MortgageContainer
